36eb339a842add911525eeed5ef889398b54423f
Carlos Gizbert <carlos.gizbert@fcamara.com.br>
2022-08-27 18:56:34 -0300

    fix prettier

--- services/admin/schools/api.ts
+++ services/admin/schools/api.ts
@@ -1,28 +1,24 @@
-import { HEADERS } from '../index';
 import axios from 'axios'
+import { HEADERS, API_URL } from '../index'
 
 import { ISchoolDTO, ISchoolR } from './interfaces'
 
-import { API_URL } from '..'
-
 export async function getSchools() {
   const response = await axios.get<ISchoolR[]>(`${API_URL}/schools`, {
-    headers: HEADERS
-  }
-  )
+    headers: HEADERS,
+  })
 
-  const transformed = response.data.map(({ _id, ...s }) => ({ id: _id, ...s }));
+  const transformed = response.data.map(({ _id, ...s }) => ({ id: _id, ...s }))
 
   return transformed
 }
 
 export async function getSchool(id: string) {
   const response = await axios.get<ISchoolDTO>(`${API_URL}/schools/${id}`, {
-    headers: HEADERS
-  }
-  )
+    headers: HEADERS,
+  })
   const transformed: any = response.data
-  transformed['_id'] = transformed['id']
+  transformed._id = transformed.id
 
   return transformed
 }
@@ -35,18 +31,19 @@ export async function createSchool(schoolDto: Omit<ISchoolDTO, 'id'>) {
 }
 
 export async function updateSchool(schoolDto: ISchoolDTO) {
-  const response = axios.patch(`${API_URL}/schools/${schoolDto.id}`, schoolDto, {
-    headers: HEADERS,
-  })
-  return response
-}
-
-export async function deleteSchool(id: string) {
-  const response = await axios.delete(
-    `${API_URL}/schools/${id}`,
+  const response = axios.patch(
+    `${API_URL}/schools/${schoolDto.id}`,
+    schoolDto,
     {
       headers: HEADERS,
     }
   )
+  return response
+}
+
+export async function deleteSchool(id: string) {
+  const response = await axios.delete(`${API_URL}/schools/${id}`, {
+    headers: HEADERS,
+  })
   return response.data
 }
