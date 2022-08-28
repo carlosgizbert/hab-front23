2367621b2322160ab54078993eccb850682a556d
Carlos Gizbert <carlos.gizbert@fcamara.com.br>
2022-08-27 22:10:23 -0300

    [feat] card schools

--- ui/components/organisms/Card/index.tsx
+++ ui/components/organisms/Card/index.tsx
@@ -1,16 +1,28 @@
 import { Card } from './style'
 
-export default function CardSchool() {
+interface ICardSchool {
+  imageUrl: string
+  textTitle: string
+  textTag: string
+  textSub: string
+  onClick: () => void
+}
+
+export default function CardSchool({
+  imageUrl,
+  textTitle,
+  textTag,
+  textSub,
+  onClick,
+}: ICardSchool) {
   return (
-    <Card.Container>
-      <Card.Header.Image imageUrl="https://yt3.ggpht.com/1cnC4t4JxgiLkCdXgTL8nZ_EYrQQ6I6ZDYUwCuNT_yZQAoJiGPlm5-vfRw541U4vJcZ6FJNpmw=s900-c-k-c0x00ffffff-no-rj">
-        <Card.Header.Tag>Vila Tupi</Card.Header.Tag>
+    <Card.Container onClick={onClick}>
+      <Card.Header.Image imageUrl={imageUrl}>
+        <Card.Header.Tag>{textTag}</Card.Header.Tag>
       </Card.Header.Image>
       <Card.Body.Wrapper>
-        <Card.Body.Title>Twister</Card.Body.Title>
-        <Card.Body.Subtitle>
-          Avenida Presidente Kennedy, Praia Grande, São Paulo, 11709-000
-        </Card.Body.Subtitle>
+        <Card.Body.Title>{textTitle}</Card.Body.Title>
+        <Card.Body.Subtitle>{textSub}</Card.Body.Subtitle>
       </Card.Body.Wrapper>
     </Card.Container>
   )
