306647898fad634d013f4da5a0ca798070b0b37c
Carlos Gizbert <carlos.gizbert@fcamara.com.br>
2022-10-05 23:52:07 -0300

    [feat] meta seo added

--- next-seo-config.ts
+++ next-seo-config.ts
@@ -13,23 +13,36 @@ export const SEO = {
     url: URL,
     site_name: SITE_NAME,
     locale: 'pt_BR',
-    images: [
-      {
-        url: 'https://www.example.ie/og-image-01.jpg',
-        width: 800,
-        height: 600,
-        alt: 'Og Image Alt',
-        type: 'image/jpeg',
-      },
-      {
-        url: 'https://www.example.ie/og-image-02.jpg',
-        width: 900,
-        height: 800,
-        alt: 'Og Image Alt Second',
-        type: 'image/jpeg',
-      },
-      { url: 'https://www.example.ie/og-image-03.jpg' },
-      { url: 'https://www.example.ie/og-image-04.jpg' },
-    ],
   },
 }
+
+// export const SEO = {
+//   title: TITLE,
+//   description: DESCRIPTION,
+//   canonical: URL,
+//   openGraph: {
+//     title: TITLE,
+//     description: DESCRIPTION,
+//     url: URL,
+//     site_name: SITE_NAME,
+//     locale: 'pt_BR',
+//     images: [
+//       {
+//         url: 'https://www.example.ie/og-image-01.jpg',
+//         width: 800,
+//         height: 600,
+//         alt: 'Og Image Alt',
+//         type: 'image/jpeg',
+//       },
+//       {
+//         url: 'https://www.example.ie/og-image-02.jpg',
+//         width: 900,
+//         height: 800,
+//         alt: 'Og Image Alt Second',
+//         type: 'image/jpeg',
+//       },
+//       { url: 'https://www.example.ie/og-image-03.jpg' },
+//       { url: 'https://www.example.ie/og-image-04.jpg' },
+//     ],
+//   },
+// }
